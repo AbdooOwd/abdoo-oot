e3af5af2a42910290130dd237ce24b4dc2faea99
AbdooOwd <benari102@gmail.com>
2024-09-21 13:18:18 +0100

    human object

---
+++ mod_assets/objects/object_human/gHumanDL.h
@@ -0,0 +1,5 @@
+extern Vtx gHumanDL_gHumanDL_mesh_layer_Opaque_vtx_cull[8];
+extern Vtx gHumanDL_gHumanDL_mesh_layer_Opaque_vtx_0[320];
+extern Gfx gHumanDL_gHumanDL_mesh_layer_Opaque_tri_0[];
+extern Gfx mat_gHumanDL_white_layerOpaque[];
+extern Gfx gHumanDL[];
